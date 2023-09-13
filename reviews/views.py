222fe8bf9cfc9e4ec7e6f0f41c7fc1b8daefb548
Béryl HOUESSOU <berylhoe@gmail.com>
2023-09-13 21:04:16 +0100

    End of Activity 1.02 – a book search scaffold so end of chapter 1 too

--- reviews/views.py
+++ reviews/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 def index(request):
     name = "World"
     return render(request, 'base.html', {'name': name})
+
+def book_search(request):
+    search = request.GET.getlist('search') or ['No search term']
+    return render(request, 'search-results.html', {'search': ' '.join(search)})
