56b668a65c315bca959995343c7f78956492e7a8
Béryl HOUESSOU <berylhoe@gmail.com>
2023-09-17 22:10:57 +0100

    Add Welcome_view View

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
 def index(request):
     name = "World"
     return render(request, 'base.html', {'name': name})
@@ -6,3 +8,9 @@ def index(request):
 def book_search(request):
     search = request.GET.getlist('search') or ['No search term']
     return render(request, 'search-results.html', {'search': ' '.join(search)})
+
+def welcome_view(request):
+    message = (f"<html><h1>Welcome to Bookr!</h1>"
+               f"<p>{Book.objects.count()} books and counting!</p>"
+               f"</html>")
+    return HttpResponse(message)
