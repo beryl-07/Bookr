277b918a293f71c9db78bec33320cb7998e07151
Béryl HOUESSOU <berylhoe@gmail.com>
2023-09-18 20:56:31 +0100

    End Exercise 3.03 – displaying a list of books and reviews

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.welcome_view, name='welcome_index'),
+    #path('', views.welcome_view, name='welcome_index'),
+    path('books/', views.books_list, name='books_list'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
 from .models import Book
-#def index(request):
-#    name = "World"
-#    return render(request, 'base.html', {'name': name})
+from .utils import average_rating
 
-#def book_search(request):
-#    search = request.GET.getlist('search') or ['No search term']
-#    return render(request, 'search-results.html', {'search': ' '.join(search)})
+def index(request):
+    return render(request, "base.html")
 
-def welcome_view(request):
-    return render(request, 'base.html')
+def book_search(request):
+    search_text = request.GET.get("search", "")
+    return render(request, "reviews/search-results.html", {"search_text": search_text})
+
+def books_list(request):
+    books = Book.objects.all()
+    book_list = []
+    for book in books:
+        reviews = book.review_set.all()
+        if reviews:
+            book_rating = average_rating([review.rating for review in reviews])
+            number_of_reviews = len(reviews)
+        else:
+            book_rating = 0
+            number_of_reviews = 0
+        book_list.append({
+            "book": book,
+            "book_rating": book_rating,
+            "number_of_reviews": number_of_reviews,
+        })
+    context = {
+        "book_list": book_list,
+    }
+    return render(request, "reviews/books_list.html", context)
