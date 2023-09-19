bbf53e5b15a3e5284130b00492d606d9f8aa1332
Béryl HOUESSOU <berylhoe@gmail.com>
2023-09-19 05:52:40 +0100

    End of Exercise 3.04 – adding template inheritance and a Bootstrap navigation bar

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.welcome_view, name='welcome_index'),
+    path('', views.index, name='index'),
     path('books/', views.books_list, name='books_list'),
 ]
