897e1184d08663a7257c2dc0a1146ef6cfc67015
Béryl HOUESSOU <berylhoe@gmail.com>
2023-09-19 06:28:21 +0100

    End of Activity 3.01 – implementing the book details view

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.books_list, name='books_list'),
+    path('book-detail/<int:book_id>/', views.book_detail, name='book_detail'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -30,3 +30,17 @@ def books_list(request):
         "book_list": book_list,
     }
     return render(request, "reviews/books_list.html", context)
+def book_detail(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    reviews = book.review_set.all()
+    if reviews:
+        book_rating = average_rating([review.rating for review in reviews])
+    else:
+        book_rating = 0
+        reviews: []
+    context = {
+        "book": book,
+        "book_rating": book_rating,
+        "reviews": reviews,
+    }
+    return render(request, "reviews/book_detail.html", context)
